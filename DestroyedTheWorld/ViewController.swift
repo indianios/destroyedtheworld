38cf81641d833f68d7b584cde68b8c250e102798
Amjad Ali <amjad.ali@angloenterprises.com>
2018-10-02 15:50:03 +0400

    Initial Commit

---
+++ DestroyedTheWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DestroyedTheWorld
+//
+//  Created by user on 02/10/2018.
+//  Copyright © 2018 user. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
